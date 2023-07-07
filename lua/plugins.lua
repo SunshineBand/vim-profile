47db67a01a152d9daad896d5ffd9c597cb1cde6a
SunshineBand <kyledchenoweth@gmail.com>
2023-07-08 00:20:13 +1000

    inital setup for github integration gh.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,6 +3,10 @@ return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
 
   -- Tools
+  use {
+    'ldelossa/gh.nvim',
+    requires = { { 'ldelossa/litee.nvim' } }
+  }
   use({
       "iamcco/markdown-preview.nvim",
       run = function() vim.fn["mkdp#util#install"]() end,
