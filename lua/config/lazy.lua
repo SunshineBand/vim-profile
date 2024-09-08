a678ee5804b06d1ea7ca3b7e8b24b65041c42988
SunshineBand <kyledchenoweth@gmail.com>
2024-09-08 12:38:16 +1000

    feat: leetcode ui, will track on neetcode

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -104,6 +104,19 @@ require('lazy').setup({
         },
         { 'folke/tokyonight.nvim' },
         { 'ap/vim-css-color' },
+        {
+            'kawre/leetcode.nvim',
+            build = ':TSUpdateSync',
+            dependencies = {
+                'nvim-telescope/telescope.nvim',
+                'nvim-lua/plenary.nvim', -- required by telescope
+                'MunifTanjim/nui.nvim',
+            },
+            opts = {
+                arg = "leetcode.nvim",
+                lang = "typescript"
+            },
+        },
     },
-    -- install = { colorscheme = { 'tokyonight.nvim' } },
+    install = { colorscheme = { 'tokyonight-storm' } },
 })
