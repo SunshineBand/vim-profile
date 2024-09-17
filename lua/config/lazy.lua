1a81d7295b4bfbf0a0f15d89f4853f33c2f3cacd
SunshineBand <kyledchenoweth@gmail.com>
2024-09-17 22:34:33 +1000

    style: format lazy file

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,13 +1,13 @@
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
+            { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+            { out,                            'WarningMsg' },
+            { '\nPress any key to exit...' },
         }, true, {})
         vim.fn.getchar()
         os.exit(1)
@@ -22,8 +22,8 @@ require('lazy').setup({
         { 'petertriho/nvim-scrollbar', opts = {} },
         {
             'nvim-telescope/telescope.nvim',
-            version = "0.1.8",
-            dependencies = {'nvim-lua/plenary.nvim'},
+            version = '0.1.8',
+            dependencies = { 'nvim-lua/plenary.nvim' },
             opts = {
                 defaults = {
                     file_ignore_patterns = {
@@ -36,7 +36,7 @@ require('lazy').setup({
         },
         {
             'ThePrimeagen/harpoon',
-            dependencies = {'nvim-lua/plenary.nvim'}
+            dependencies = { 'nvim-lua/plenary.nvim' }
         },
         { 'tpope/vim-fugitive' },
         {
@@ -54,7 +54,7 @@ require('lazy').setup({
                     -- Navigation
                     map('n', ']h', function()
                         if vim.wo.diff then
-                            vim.cmd.normal({']h', bang = true})
+                            vim.cmd.normal({ ']h', bang = true })
                         else
                             gitsigns.nav_hunk('next')
                         end
@@ -62,7 +62,7 @@ require('lazy').setup({
 
                     map('n', '[h', function()
                         if vim.wo.diff then
-                            vim.cmd.normal({'[h', bang = true})
+                            vim.cmd.normal({ '[h', bang = true })
                         else
                             gitsigns.nav_hunk('prev')
                         end
@@ -71,20 +71,20 @@ require('lazy').setup({
                     -- Actions
                     map('n', '<leader>hs', gitsigns.stage_hunk)
                     map('n', '<leader>hr', gitsigns.reset_hunk)
-                    map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-                    map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+                    map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+                    map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
                     map('n', '<leader>hS', gitsigns.stage_buffer)
                     map('n', '<leader>hu', gitsigns.undo_stage_hunk)
                     map('n', '<leader>hR', gitsigns.reset_buffer)
                     map('n', '<leader>hp', gitsigns.preview_hunk)
-                    map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
+                    map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
                     map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
                     map('n', '<leader>hd', gitsigns.diffthis)
                     map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
                     map('n', '<leader>td', gitsigns.toggle_deleted)
 
                     -- Text object
-                    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+                    map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
                 end
             }
         },
@@ -117,23 +117,14 @@ require('lazy').setup({
             }
             -- config = function() require('oil').init() end
         },
-        {
-            'akinsho/git-conflict.nvim', version = "*",
-            opts = {}
-        },
+        { 'akinsho/git-conflict.nvim',         version = '*', opts = {} },
         { 'Almo7aya/openingh.nvim' },
         { 'github/copilot.vim' },
 
         -- Lsp
 
-        {
-            'williamboman/mason.nvim',
-            opts = {}
-        },
-        {
-            'williamboman/mason-lspconfig.nvim',
-            opts = {}
-        },
+        { 'williamboman/mason.nvim',           opts = {} },
+        { 'williamboman/mason-lspconfig.nvim', opts = {} },
         { 'neovim/nvim-lspconfig' },
         { 'hrsh7th/cmp-nvim-lsp' },
         { 'hrsh7th/cmp-buffer' },
@@ -146,7 +137,7 @@ require('lazy').setup({
         {
             'nvim-treesitter/nvim-treesitter',
             config = function()
-                local configs = require'nvim-treesitter.configs'
+                local configs = require 'nvim-treesitter.configs'
 
                 configs.setup {
                     ensure_installed = { 'typescript', 'tsx', 'ruby' },
@@ -179,7 +170,7 @@ require('lazy').setup({
 
         { 'adelarsq/vim-devicons-emoji' },
         { 'nvim-telescope/telescope-symbols.nvim' },
-        { 'catppuccin/nvim', name = 'catppuccin' },
+        { 'catppuccin/nvim',                        name = 'catppuccin' },
         { 'nerdypepper/agila.vim' },
         { 'ellisonleao/gruvbox.nvim' },
         {
@@ -203,8 +194,8 @@ require('lazy').setup({
                 'MunifTanjim/nui.nvim',
             },
             opts = {
-                arg = "leetcode.nvim",
-                lang = "python3"
+                arg = 'leetcode.nvim',
+                lang = 'python3'
             },
         },
     },
